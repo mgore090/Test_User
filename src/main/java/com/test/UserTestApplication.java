22c0c68c055a4bb168445036cfd92e70a327d62f
mgore090 <83571674+mgore090@users.noreply.github.com>
2024-03-01 17:58:01 +0530

    Update UserTestApplication.java

--- src/main/java/com/test/UserTestApplication.java
+++ src/main/java/com/test/UserTestApplication.java
@@ -3,7 +3,7 @@ package com.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class UserTestApplication {
 
 	public static void main(String[] args) {
